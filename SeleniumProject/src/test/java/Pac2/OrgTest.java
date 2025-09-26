c5874c1382cc1f7fccd6a7a790cf28d9d8747c24
Swathi <Swathi@DESKTOP-KUK2QS1>
2025-09-26 14:59:42 +0530

    commit

---
+++ SeleniumProject/src/test/java/Pac2/OrgTest.java
@@ -0,0 +1,16 @@
+package Pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createorgTest()
+	{
+	System.out.println("execute createcontactTest");	
+	}
+	@Test
+	public void modifyorgTest()
+	{
+	System.out.println("execute modifycontactTest");	
+	}
+}
